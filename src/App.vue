495f2236a14eaa166f91cf41024c6abcb78fdd7c
Xukai1666666 <2684024189@qq.com>
2022-09-05 23:40:55 +0800

    feat:导入 genshin keli

--- src/App.vue
+++ src/App.vue
@@ -2,10 +2,11 @@
 // This starter template is using Vue 3 <script setup> SFCs
 // Check out https://vuejs.org/api/sfc-script-setup.html#script-setup
 import HelloWorld from './components/HelloWorld.vue'
+import Keli from './components/keli.vue'
 </script>
 
 <template>
-  <HelloWorld msg="Vite + Vue" />
+  <Keli />
 </template>
 
 <style scoped>
---
+++ src/components/keli.vue
@@ -0,0 +1,40 @@
+<script  setup lang="ts">
+import * as THREE from 'three' 
+import { onMounted  } from 'vue'
+import {MMDLoader} from 'three/examples/jsm/loaders/MMDLoader'
+const scene = new THREE.Scene()
+const width = window.innerWidth, height = window.innerHeight  ,
+      camera = new THREE.PerspectiveCamera(45, width/height, 1, 1000)
+camera.position.set(200,200,200) 
+camera.lookAt(0,0,0)
+ // Instantiate a loader
+const loader = new MMDLoader();
+THREE.DefaultLoadingManager
+// Load a MMD model
+loader.load(
+	// path to PMD/PMX file
+	'src/assets/可莉/可莉2.0.pmx',
+	// called when the resource is loaded
+	function ( mesh ) {
+
+		scene.add( mesh );
+
+	},
+	// called when loading is in progresses
+	function ( xhr ) {
+
+		console.log( ( xhr.loaded / xhr.total * 100 ) + '% loaded' );
+
+	},
+	// called when loading has errors
+	function ( error ) {
+
+		console.log( 'An error happened' );
+
+	}
+);
+
+</script>
+<template>
+    <div id="keli"></div>
+</template>
